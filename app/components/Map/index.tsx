70e95404b37d97bd7ef9e36dea43a1e2032df6e5
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2025-11-22 11:17:16 -0300

    Map with controls

--- app/components/Map/index.tsx
+++ app/components/Map/index.tsx
@@ -1,41 +1,9 @@
-"use client";
+import MapComponent from "../ui/Map";
 
-import { useState, useEffect } from "react";
-import Content from "./Content";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const Map = () => {
-  const [initialLatitude, setInitialLatitude] = useState<number | undefined>(
-    undefined
-  );
-  const [initialLongitude, setInitialLongitude] = useState<number | undefined>(
-    undefined
-  );
-  const [initialZoom, setInitialZoom] = useState<number | undefined>(undefined);
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setInitialLatitude(position.coords.latitude);
-          setInitialLongitude(position.coords.longitude);
-          setInitialZoom(10);
-        },
-        (error) => {
-          console.error("Error getting user location:", error);
-        }
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  }, []);
-
-  return (
-    <Content
-      initialLatitude={initialLatitude}
-      initialLongitude={initialLongitude}
-      initialZoom={initialZoom}
-    />
-  );
+  return <MapComponent />;
 };
 
 export default Map;
--- app/components/ui/Map.tsx
+++ app/components/ui/Map.tsx
@@ -1,26 +1,61 @@
-import MapBox from "react-map-gl/mapbox";
+"use client";
+
 import "mapbox-gl/dist/mapbox-gl.css";
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
 
-type ViewState = {
-  longitude: number;
-  latitude: number;
-  zoom: number;
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+const Map = () => {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-70.6693, -33.4489],
+      zoom: 12,
+    });
+
+    // Render custom marker components
+    /* geoJson.features.forEach((feature) => {
+      // Create a React ref
+      const ref = React.createRef();
+      // Create a new DOM node and save it to the React ref
+      ref.current = document.createElement('div');
+      // Render a Marker Component on our new DOM node
+      createRoot(ref.current).render(
+        <Marker onClick={markerClicked} feature={feature} />
+      );
+
+      // Create a Mapbox Marker at our new DOM node
+      new mapboxgl.Marker(ref.current)
+        .setLngLat(feature.geometry.coordinates)
+        .addTo(map);
+    }); */
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+      })
+    );
+
+    return () => map.remove();
+  }, []);
+
+  const markerClicked = (title: string) => {
+    window.alert(title);
+  };
+
+  return <div className="w-100 h-100" ref={mapContainerRef} />;
 };
 
-type Props = {
-  viewState: ViewState;
-  setViewState: (viewState: ViewState) => void;
-};
-
-const MapComponent = ({ viewState, setViewState }: Props) => (
-  <div className="w-80 h-80">
-    <MapBox
-      {...viewState}
-      onMove={(evt) => setViewState(evt.viewState)}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken="pk.eyJ1IjoidmljdG9ycGF0byIsImEiOiJja2h3dHdhMW8wM3hwMnFxejJxcXRpNHF3In0.mioBmmIYaiRDBbdyXo36qA"
-    />
-  </div>
-);
-
-export default MapComponent;
+export default Map;
