12e9c87b7d1472e5dda704d06767a09a21bc8536
mvargas33 <mvargas.uchile@gmail.com>
2025-11-23 06:00:34 -0300

    💄Minot change

--- app/components/DiscussionCard.tsx
+++ app/components/DiscussionCard.tsx
@@ -118,7 +118,7 @@ function MessageList({
         <input
           type="text"
           className="flex-1 px-3 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-300"
-          placeholder="My comment ..."
+          placeholder="Opino que ..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
           style={{
